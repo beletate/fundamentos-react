eec619792416eb4588a27a429cc2fc6b149c813e
Vinicius Beletate <vinicius_beletate@hotmail.com>
2021-03-24 11:18:10 -0300

    Adicionando contador e redistribuindo componentes

--- src/App.jsx
+++ src/App.jsx
@@ -15,61 +15,76 @@ import CondicionalComIf from './components/basicos/CondicionalComIf'
 import Pai from './components/comunicacao/direta/Pai'
 import Super from './components/comunicacao/indireta/Super'
 
+import Input from './components/form/input'
+
+import Contador from './components/contador/Contador'
+
 
 
 export default props => (
     <div className="App">
+        <h1>Fundamentos React</h1>
 
-        <Card titulo="#07 - Comunicação indireta" color="#000">
-            <Super></Super>
+        <div className="Cards">
+
+            <Card titulo="#10 - Contador" color="#4A4F19">
+                <Contador passo={10} valor={100}></Contador>
+            </Card>
+
+            <Card titulo="#09 - Input" color="#9C0F5F">
+                <Input></Input>
+            </Card>
+
+            <Card titulo="#08 - Comunicação indireta" color="#000">
+                <Super></Super>
+            </Card>
+
+            <Card titulo="#07 - Comunicação direta" color="#4298B5">
+                <Pai sobrenome="Freitas"></Pai>
+            </Card>
+
+            <Card titulo="#06 - Component condicional v2" color="#FA6900">
+                <CondicionalComIf numero={10} />
+            </Card>
+
+            <Card titulo="#05 - Component condicional v1" color="#E94C6F">
+                <Condicional numero={10} />
+            </Card>
+
+            <Card titulo="#04 - Component de repetição" color="#008BBA">
+                <Repeticao />
+            </Card>
+
+            <Card titulo="#03 - Component com parametros" color="#D96459">
+
+                <ComParametro
+                    titulo="Esse é o titulo"
+                    subtitulo="Esse é um substitulo" />
+                <ComParametro
+                    titulo="Opa"
+                    subtitulo="Epa" />
+            </Card>
+
+            <Card titulo="#02 - Component com filhos" color="#FF85CB">
+
+                <ComFilhos>
+                    <ul>
+                        <li>Ana</li>
+                        <li>Bia</li>
+                        <li>Carlos</li>
+                        <li>Daniel</li>
+                    </ul>
+                </ComFilhos>
+            </Card>
+
+            <Card titulo='#01 - Primeiro Component' color="#92B06A">
+                <Primeiro />
+            </Card>
+
+            <Card titulo='Exercícios X'>
+                Conteudo
         </Card>
-
-        <Card titulo="#07 - Comunicação direta" color="#4298B5">
-            <Pai sobrenome="Freitas"></Pai>
-        </Card>
-
-        <Card titulo="#06 - Component condicional v2" color="#FA6900">
-            <CondicionalComIf numero={10} />
-        </Card>
-
-        <Card titulo="#05 - Component condicional v1" color="#E94C6F">
-            <Condicional numero={10} />
-        </Card>
-
-        <Card titulo="#04 - Component de repetição" color="#008BBA">
-            <Repeticao />
-        </Card>
-
-        <Card titulo="#03 - Component com parametros" color="#D96459">
-
-            <ComParametro
-                titulo="Esse é o titulo"
-                subtitulo="Esse é um substitulo" />
-            <ComParametro
-                titulo="Opa"
-                subtitulo="Epa" />
-        </Card>
-
-        <Card titulo="#02 - Component com filhos" color="#FF85CB">
-
-            <ComFilhos>
-                <ul>
-                    <li>Ana</li>
-                    <li>Bia</li>
-                    <li>Carlos</li>
-                    <li>Daniel</li>
-                </ul>
-            </ComFilhos>
-        </Card>
-
-        <Card titulo='#01 - Primeiro Component' color="#92B06A">
-            <Primeiro />
-        </Card>
-
-        <Card titulo='Exercícios X'>
-            Conteudo
-        </Card>
-
+        </div>
 
     </div>
 )
