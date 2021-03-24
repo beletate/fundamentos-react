8a467c219592f092b2d1dfa708877a500e01d8f5
Vinicius Beletate <vinicius_beletate@hotmail.com>
2021-03-24 10:06:48 -0300

    Comunicacao direta e indireta entre componentes pai e filho

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,45 @@ import React from 'react'
 
 import './App.css'
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import ComFilhos from './components/basicos/ComFilhos'
 
 import Card from './components/layout/Card'
 
-import Repeticao from './components/Repeticao'
-import Condicional from './components/Condicional'
-import CondicionalComIf from './components/CondicionalComIf'
+import Repeticao from './components/basicos/Repeticao'
+import Condicional from './components/basicos/Condicional'
+import CondicionalComIf from './components/basicos/CondicionalComIf'
+
+import Pai from './components/comunicacao/direta/Pai'
+import Super from './components/comunicacao/indireta/Super'
 
 
 
 export default props => (
     <div className="App">
 
-        <Card titulo="#05 - Component condicional v2">
+        <Card titulo="#07 - Comunicação indireta" color="#000">
+            <Super></Super>
+        </Card>
+
+        <Card titulo="#07 - Comunicação direta" color="#4298B5">
+            <Pai sobrenome="Freitas"></Pai>
+        </Card>
+
+        <Card titulo="#06 - Component condicional v2" color="#FA6900">
             <CondicionalComIf numero={10} />
         </Card>
 
-        <Card titulo="#05 - Component condicional v1">
+        <Card titulo="#05 - Component condicional v1" color="#E94C6F">
             <Condicional numero={10} />
         </Card>
 
-        <Card titulo="#04 - Component de repetição">
+        <Card titulo="#04 - Component de repetição" color="#008BBA">
             <Repeticao />
         </Card>
 
-        <Card titulo="#03 - Component com parametros">
+        <Card titulo="#03 - Component com parametros" color="#D96459">
 
             <ComParametro
                 titulo="Esse é o titulo"
@@ -39,7 +50,7 @@ export default props => (
                 subtitulo="Epa" />
         </Card>
 
-        <Card titulo="#02 - Component com filhos">
+        <Card titulo="#02 - Component com filhos" color="#FF85CB">
 
             <ComFilhos>
                 <ul>
@@ -51,7 +62,7 @@ export default props => (
             </ComFilhos>
         </Card>
 
-        <Card titulo='#01 - Primeiro Component'>
+        <Card titulo='#01 - Primeiro Component' color="#92B06A">
             <Primeiro />
         </Card>
 
